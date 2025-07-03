b75c99ac575cec09acc5600d083a6b5506cd2ac8
Bhupesh517 <reddybhupesh71@gmail.com>
2025-07-03 18:42:08 +0530

    pra

---
+++ pass or fail.py
@@ -0,0 +1,5 @@
+stu_marks=int(input("Enter the Marks :"))
+if stu_marks>=40 :
+    print("PASSED")
+else:
+    print("FAIL")
---
+++ score.py
@@ -0,0 +1,9 @@
+score = int(input("score:"))
+if score >= 90 :
+    print("exlellent")
+
+elif score >=75 <= 89 :
+    print ("good")
+
+elif score >=50 <=74 :
+    print("fair")
