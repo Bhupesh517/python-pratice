be2162ebf52ce6b0b5c443cfd9f4bc131a1848e7
Bhupesh517 <reddybhupesh71@gmail.com>
2025-07-03 18:40:29 +0530

    delet

--- score.py
+++
@@ -1,9 +0,0 @@
-score = int(input("score:"))
-if score >= 90 :
-    print("exlellent")
-
-elif score >=75 <= 89 :
-    print ("good")
-
-elif score >=50 <=74 :
-    print("fair")
--- vote eligibility check.py
+++
@@ -1,7 +0,0 @@
-("age = 19")
-age = int(input("age :"))
-if age < 18 :
-    print ("not eligibil")
-else :
-    print (" eligibil")
-
